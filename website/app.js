4b7e249aa02405b51f10e7cdf0240fa8e9c472de
ChristaPigeon <cstath26@gmail.com>
2021-03-31 12:08:02 -0700

    feat: Write async getWeather function client side

--- website/app.js
+++ website/app.js
@@ -1,5 +1,5 @@
 /* Global Variables */
-const baseURL = "api.openweathermap.org/data/2.5/weather?zip=";
+const baseURL = "http://api.openweathermap.org/data/2.5/weather?zip=";
 const apiKey = ",us&appid=9aae1adc99b0c613588bbe571588031d";
 
 // Create a new date instance dynamically with JS
@@ -9,11 +9,18 @@ let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 // Event Listener - Click on Element with ID 'generate'
 document.getElementById('generate').addEventListener('click', performAction)
 
+// Event Listener Callback Function: performAction
+function performAction(event) {
+  const zip = document.getElementById('zip').value;
+  getWeather(baseURL, zip, apiKey);
+}
+
 // Async function that uses fetch() to make a GET request to the OpenWeatherMap API
-const getWeather = async(baseURL, zip, apiKey) => {
-  const res = await fetch(baseURL+zip+apiKey)
+const getWeather = async (baseURL, zip, apiKey) => {
+  const res = await fetch(baseURL+zip+apiKey);
   try {
     const data = await res.json();
+    console.log(data);
     return data;
   } catch(error) {
     console.log("error", error);
