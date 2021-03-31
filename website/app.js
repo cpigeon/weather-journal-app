4df9f33b53cd9cd39bb74da121a7bb078256e96f
ChristaPigeon <cstath26@gmail.com>
2021-03-30 17:33:00 -0700

    feat: Setup GET Request to OpenWeatherMap API

--- website/app.js
+++ website/app.js
@@ -1,8 +1,18 @@
 /* Global Variables */
 const baseURL = "api.openweathermap.org/data/2.5/weather?zip=";
-const countryCode = ',us&appid='
-const apiKey = "9aae1adc99b0c613588bbe571588031d";
+const apiKey = ",us&appid=9aae1adc99b0c613588bbe571588031d";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Async function that uses fetch() to make a GET request to the OpenWeatherMap API
+const getWeather = async(baseURL, zip, apiKey) => {
+  const res = await fetch(baseURL+zip+apiKey)
+  try {
+    const data = await res.json();
+    return data;
+  } catch(error) {
+    console.log("error", error);
+  }
+}
