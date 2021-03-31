2b4107b60a12dea8ce1c03466e68c41bda16a8eb
ChristaPigeon <cstath26@gmail.com>
2021-03-30 17:35:59 -0700

    feat: Add Event Listener for clicking generate

--- website/app.js
+++ website/app.js
@@ -6,6 +6,9 @@ const apiKey = ",us&appid=9aae1adc99b0c613588bbe571588031d";
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
+// Event Listener - Click on Element with ID 'generate'
+document.getElementById('generate').addEventListener('click', performAction)
+
 // Async function that uses fetch() to make a GET request to the OpenWeatherMap API
 const getWeather = async(baseURL, zip, apiKey) => {
   const res = await fetch(baseURL+zip+apiKey)
