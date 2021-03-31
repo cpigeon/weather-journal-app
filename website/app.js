2272cd3eb771b9e56fe9f308b88224e7ed6c1677
ChristaPigeon <cstath26@gmail.com>
2021-03-30 17:25:17 -0700

    feat: Create variables for OpenWeatherMap API

--- website/app.js
+++ website/app.js
@@ -1,4 +1,7 @@
 /* Global Variables */
+const baseURL = "api.openweathermap.org/data/2.5/weather?zip=";
+const countryCode = ',us&appid='
+const apiKey = "9aae1adc99b0c613588bbe571588031d";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
